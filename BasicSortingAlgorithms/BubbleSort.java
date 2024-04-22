327e6d2303adaff08aaf0fba67000ad3354772f2
Kazim Hasani <smkazimhasani@gmail.com>
2024-04-21 19:48:02 -0700

    Practice Questions

---
+++ BasicSortingAlgorithms/BubbleSort.java
@@ -0,0 +1,27 @@
+package BasicSortingAlgorithms;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+        bubble(arr);
+        printArr(arr);
+    }
+
+    public static void bubble(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ BasicSortingAlgorithms/SelectionSort.java
@@ -0,0 +1,29 @@
+package BasicSortingAlgorithms;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+        selection(arr);
+        printArr(arr);
+    }
+
+    public static void selection(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int maxPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[maxPos] < arr[j]) {
+                    maxPos = j;
+                }
+            }
+            int temp = arr[maxPos];
+            arr[maxPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
